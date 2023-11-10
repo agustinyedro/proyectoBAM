9e3910bf77e03ac2e751f17fbfba18230516114b
Agustin Yedro <agustinyedro@gmail.com>
2023-11-10 10:42:50 -0300

    agree elementos

--- script/script.js
+++ script/script.js
@@ -1,23 +1,23 @@
- //  seleccionamos los dos elementos que serán clickables
+//  seleccionamos los dos elementos que serán clickables
 
- const botonBurger = document.getElementById("button-menu");
- const nuevaNav = document.getElementById("nav");
+const botonBurger = document.getElementById("button-menu");
+const nuevaNav = document.getElementById("nav");
 
- /* 
+/* 
    cada ves que se haga click en el botón 
    agrega y quita las clases necesarias 
    para que el menú se muestre.
  */
- botonBurger.addEventListener("click", () => {
-   botonBurger.classList.toggle("close");
-   nuevaNav.classList.toggle("show");
- });
+botonBurger.addEventListener("click", () => {
+  botonBurger.classList.toggle("close");
+  nuevaNav.classList.toggle("show");
+});
 
 /* 
   Cuándo se haga click fuera del contenedor se esconderse.
 */
 
-nuevaNav.addEventListener("click", e => {
+nuevaNav.addEventListener("click", (e) => {
   if (e.target.id === "nav") {
     nuevaNav.classList.remove("show");
     botonBurger.classList.remove("close");
@@ -26,23 +26,39 @@ nuevaNav.addEventListener("click", e => {
 
 let posAnterior = window.pageYOffset;
 
-window.addEventListener('scroll', function() {
-  var navbar = document.querySelector('.main-header');
+window.addEventListener("scroll", function () {
+  var navbar = document.querySelector(".main-header");
   var posActual = window.pageYOffset;
 
   if (posAnterior > posActual) {
     // El usuario está desplazándose hacia arriba, muestra la barra de navegación
-    navbar.classList.remove('hidden');
+    navbar.classList.remove("hidden");
   } else {
     // El usuario está desplazándose hacia abajo, oculta la barra de navegación
-    navbar.classList.add('hidden');
+    navbar.classList.add("hidden");
   }
 
   posAnterior = posActual;
 });
 
+
+const botonMap = document.getElementById("iconos-contactos-boton");
+const formulario = document.getElementById("formulario");
+
+if (botonMap && formulario) {
+  botonMap.addEventListener("click", e => {
+    e.preventDefault(); 
+      formulario.classList.toggle("mostrar");
+      botonMap.classList.toggle("mostrar");
+      formulario.scrollIntoView({
+        behavior: 'smooth', // Puedes cambiar 'smooth' por 'auto' para un scroll instantáneo
+        block: 'start' // Puedes cambiar 'start' por 'end' o 'center' según tu preferencia
+      });
+  });
+}
+
 // document.getElementById("whatsapp-icon").addEventListener("click", function() {
-  
+
 //   var phoneNumber = '+54351';
 //   var message = 'Hola en que podemos ayudarte?';
 
@@ -50,16 +66,3 @@ window.addEventListener('scroll', function() {
 
 //   window.open(whatsappURL, '_blank');
 // });
-
- 
-
-
-
-
-
-
-
-
-
-
-
