2519b3775f25493d7a6461f89fc374fc6d0b141b
Agustin Yedro <76633203+agustinyedro@users.noreply.github.com>
2024-05-12 02:35:01 -0300

    mas estilos para destkop

--- logic/script.js
+++ logic/script.js
@@ -1,5 +1,6 @@
 window.addEventListener("scroll", function () {
   const $nav = document.getElementById("header");
+  // console.log(window.scrollY);
   if (window.scrollY > 120) {
     $nav.classList.add("nav-scroll");
   } else if (this.window.scrollY < 49) {
@@ -37,13 +38,55 @@ function previousImage() {
 document.querySelector(".btn-left").addEventListener("click", previousImage);
 document.querySelector(".btn-right").addEventListener("click", nextImage);
 
-window.addEventListener('scroll', function () {
-  var seccionFija = document.getElementById('caracteristicas');
-  var seccionFijaPos = seccionFija.offsetTop;
 
-  if (window.pageYOffset >= seccionFijaPos) {
+/************* ANIMACION CARACTERISTICAS *********/
+
+let seccionFija = document.getElementById('caracteristicas');
+let seccionFijaPos = seccionFija.offsetTop;
+
+const umbral = 100;
+
+window.addEventListener('scroll', function () {
+  // console.log(window.pageYOffset);
+  if (window.scrollY > (seccionFijaPos - umbral)) {
     seccionFija.classList.add('fixed');
+
   } else {
     seccionFija.classList.remove('fixed');
   }
 });
+
+let seccionFija2 = document.getElementById('sobre-nosotros');
+let seccionFijaPos2 = seccionFija2.offsetTop - 100;
+
+
+window.addEventListener('scroll', function () {
+
+  let porcentaje = Math.round(seccionFijaPos2 / this.window.scrollY * 100)
+
+  console.log(porcentaje);
+
+  // console.log(seccionFijaPos2 - this.window.scrollY);
+  if (porcentaje < 134) {
+    seccionFija2.style.width = porcentaje - 10 + '%';
+    if (porcentaje < 114) {
+      seccionFija2.style.borderRadius = '2.5rem';
+      seccionFija2.classList.add('fixed');
+    } else {
+      seccionFija2.style.borderRadius = '0%';
+      seccionFija2.classList.remove('fixed');
+    }
+    seccionFija2.classList.add('fixed');
+    if (porcentaje < 90) {
+      seccionFija.style.color = 'white';
+    } else {
+      seccionFija.style.color = 'hsl(120, 100%, 0%)';
+    }
+  } else {
+
+    seccionFija2.classList.remove('fixed');
+  }
+})
+
+
+
