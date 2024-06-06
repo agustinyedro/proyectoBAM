7d363f06fb0a26bb8dd8f8b5ada64b33afc165b1
Agustin Yedro <76633203+agustinyedro@users.noreply.github.com>
2024-06-06 17:04:53 -0300

    algunos cambios antes de hacerlo node

---
+++ logic/imagenes.js
@@ -0,0 +1,5 @@
+
+export const galeriaImagnes = ($imagenes) => {
+    $imagenes.innerHTML = /* HTML */`
+    <img src="./public/img/cabañafrente.webp" alt="cabaña" class="img-grande">`;
+}
--- logic/script.js
+++ logic/script.js
@@ -1,118 +1,70 @@
-window.addEventListener("scroll", function () {
-  const $nav = document.getElementById("header");
-  // console.log(window.scrollY);
-  if (window.scrollY > 120) {
-    $nav.classList.add("nav-scroll");
-  } else if (this.window.scrollY < 49) {
-    $nav.classList.remove("nav-scroll");
-  }
-});
-
+/******** CARRUSEL **********/
 const carouselImages = document.querySelectorAll(".img-carrousel");
-const totalImages = carouselImages.length;
 let currentIndex = 0;
 
 function showImage(index) {
-  // Ocultar todas las imágenes
-  carouselImages.forEach((image) => {
-    image.classList.add("hidden");
-    image.classList.remove("active");
+  carouselImages.forEach(image => {
+    image.classList.toggle("hidden", true);
+    image.classList.toggle("active", false);
   });
-
-  // Mostrar la imagen correspondiente al índice
-  carouselImages[index].classList.remove("hidden");
-  carouselImages[index].classList.add("active");
+  carouselImages[index].classList.toggle("hidden", false);
+  carouselImages[index].classList.toggle("active", true);
 }
 
-function nextImage() {
-  currentIndex = (currentIndex + 1) % totalImages;
+function changeImage(increment) {
+  currentIndex = (currentIndex + increment + carouselImages.length) % carouselImages.length;
   showImage(currentIndex);
 }
 
-function previousImage() {
-  // console.log(currentIndex);
-  currentIndex = (currentIndex - 1 + totalImages) % totalImages;
-  showImage(currentIndex);
-}
+document.querySelector(".btn-left").addEventListener("click", () => changeImage(-1));
+document.querySelector(".btn-right").addEventListener("click", () => changeImage(1));
 
-document.querySelector(".btn-left").addEventListener("click", previousImage);
-document.querySelector(".btn-right").addEventListener("click", nextImage);
 
-/************* ANIMACION CARACTERISTICAS *********/
-
-let seccionFija = document.getElementById("caracteristicas");
-let seccionFijaPos = seccionFija.offsetTop;
-
+/************ SCROLL **************/
+const $seccionCaracteristicas = document.getElementById("caracteristicas");
 const umbral = 100;
 
-window.addEventListener("scroll", function () {
-  // console.log(window.pageYOffset);
-  if (window.scrollY > seccionFijaPos - umbral) {
-    seccionFija.classList.add("fixed");
-  } else {
-    seccionFija.classList.remove("fixed");
-  }
-});
+const $seccionSobreNosotros = document.getElementById("sobre-nosotros");
+const seccionSobreNosotrosPos = $seccionSobreNosotros.offsetTop - 100;
 
-let seccionFija2 = document.getElementById("sobre-nosotros");
-let seccionFijaPos2 = seccionFija2.offsetTop - 100;
+const $footer = document.getElementById("footer");
+const footerPos = $footer.offsetTop;
 
 window.addEventListener("scroll", function () {
-  let porcentaje = Math.round((seccionFijaPos2 / this.window.scrollY) * 100);
 
-  //console.log(porcentaje);
-
-  // console.log(seccionFijaPos2 - this.window.scrollY);
-  if (porcentaje < 134) {
-    seccionFija2.style.width = porcentaje - 10 + "%";
-    if (porcentaje < 114) {
-      seccionFija2.style.borderRadius = "2rem";
-      seccionFija2.classList.add("fixed");
-    } else {
-      seccionFija2.style.borderRadius = "0%";
-      seccionFija2.classList.remove("fixed");
-    }
-    seccionFija2.classList.add("fixed");
-    if (porcentaje < 90) {
-      seccionFija.style.color = "white";
-    } else {
-      seccionFija.style.color = "hsl(120, 100%, 0%)";
-    }
-  } else {
-    seccionFija2.classList.remove("fixed");
+  /**** Nav scroll ****/
+  const nav = document.getElementById("header");
+  if (window.scrollY > 120) {
+    nav.classList.add("nav-scroll");
+  } else if (window.scrollY < 49) {
+    nav.classList.remove("nav-scroll");
   }
+
+  /************ Sección Caracteristicas scroll *************/
+  if (window.scrollY > $seccionCaracteristicas.offsetTop - umbral) {
+    $seccionCaracteristicas.classList.add("fixed");
+  } else {
+    $seccionCaracteristicas.classList.remove("fixed");
+  }
+
+  /**************** Sección Sobre Nosotros scroll *****************/
+  const porcentaje = Math.round((seccionSobreNosotrosPos / window.scrollY) * 100);
+  const isFixed = porcentaje < 134;
+
+  $seccionSobreNosotros.style.width = isFixed ? (porcentaje - 10) + "%" : "";
+  $seccionSobreNosotros.style.borderRadius = isFixed && porcentaje < 114 ? "2rem" : "0%";
+  $seccionSobreNosotros.classList.toggle("fixed", isFixed);
+  $seccionCaracteristicas.style.color = porcentaje < 90 ? "white" : "hsl(120, 100%, 0%)";
+
+  /************* Sección Footer scroll *****************/
+  const porcentajeFooter = Math.round((footerPos / window.scrollY) * 100);
+  const isScroll = porcentajeFooter < 120;
+
+  $footer.style.width = isScroll ? (porcentajeFooter - 15) + "%" : "";
+  $footer.style.height = isScroll ? (porcentajeFooter - 47) + "vh" : "";
+  $footer.style.borderRadius = isScroll && porcentajeFooter < 116 ? "2rem" : "0%";
+  $footer.classList.toggle("scroll", isScroll);
+
 });
 
-let seccionFijaFooter = document.getElementById("footer");
-
-// let seccionFijaFooter2 = document.getElementById("footer-bg");
-
-// console.log(seccionFijaFooter.offsetTop);
-
-let seccionFijaPosFooter = seccionFijaFooter.offsetTop;
-
-// console.log(seccionFijaPosFooter);
-
-window.addEventListener("scroll", function () {
-  let porcentajeFooter = Math.round(
-    (seccionFijaPosFooter / this.window.scrollY) * 100
-  );
-
-  // console.log(porcentajeFooter - 20);
-
-  if (porcentajeFooter < 120) {
-    seccionFijaFooter.style.width = porcentajeFooter - 15 + "%";
-    seccionFijaFooter.style.height = porcentajeFooter - 47 + "vh";
-
-    if (porcentajeFooter < 116) {
-      seccionFijaFooter.style.borderRadius = "2rem";
-      seccionFijaFooter.classList.add("scroll");
-    } else {
-      seccionFijaFooter.style.borderRadius = "0%";
-      seccionFijaFooter.classList.remove("scroll");
-    }
-    seccionFijaFooter.classList.add("scroll");
-  } else {
-    seccionFijaFooter.classList.remove("scroll");
-  }
-});
+const $imagenes = document.getElementById("galeria-imagenes-contenido");
