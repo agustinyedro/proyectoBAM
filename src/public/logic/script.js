7842df8eec24d1550d58c01d3e25b51204075d5c
Agustin Yedro <76633203+agustinyedro@users.noreply.github.com>
2024-06-13 11:22:33 -0300

    se arregla footer

--- src/public/logic/script.js
+++ src/public/logic/script.js
@@ -30,7 +30,10 @@ const $seccionSobreNosotros = document.getElementById("sobre-nosotros");
 const seccionSobreNosotrosPos = $seccionSobreNosotros.offsetTop - 100;
 
 const $footer = document.getElementById("footer");
-const footerPos = $footer.offsetTop;
+const footerPos = $footer.offsetTop - 80;
+
+// console.log("Offset Top del Footer: ", $footer.offsetTop);
+// console.log("Posición del Footer: ", footerPos);
 
 window.addEventListener("scroll", function () {
 
@@ -42,7 +45,7 @@ window.addEventListener("scroll", function () {
     nav.classList.remove("nav-scroll");
   }
 
-  /************ Sección Caracteristicas scroll *************/
+  /************ Sección Características scroll *************/
   if (window.scrollY > $seccionCaracteristicas.offsetTop - umbral) {
     $seccionCaracteristicas.classList.add("fixed");
   } else {
@@ -60,14 +63,23 @@ window.addEventListener("scroll", function () {
 
   /************* Sección Footer scroll *****************/
   const porcentajeFooter = Math.round((footerPos / window.scrollY) * 100);
+  // console.log("ScrollY: ", window.scrollY, " Porcentaje Footer: ", porcentajeFooter);
   const isScroll = porcentajeFooter < 120;
+  // console.log(porcentajeFooter - 15);
 
-  $footer.style.width = isScroll ? (porcentajeFooter - 15) + "%" : "";
-  $footer.style.height = isScroll ? (porcentajeFooter - 47) + "vh" : "";
-  $footer.style.borderRadius = isScroll && porcentajeFooter < 116 ? "2rem" : "0%";
-  $footer.classList.toggle("scroll", isScroll);
+  const nearBottom = (window.innerHeight + scrollY) >= document.body.offsetHeight;
 
+  if (!nearBottom) {
+    $footer.style.width = isScroll ? (porcentajeFooter) + "%" : "";
+    $footer.style.height = isScroll ? (porcentajeFooter - 32) + "vh" : "";
+    $footer.style.borderRadius = isScroll && porcentajeFooter < 116 ? "2rem" : "0%";
+    $footer.classList.toggle("scroll", isScroll);
+  }
 });
+
 
 const $imagenes = document.getElementById("galeria-imagenes-contenido");
 $imagenes.innerHTML = await galeriaImagenes();
+
+
+
