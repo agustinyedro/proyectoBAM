a57129fd79693a374aa496aa28c9de35592baa71
Agustin <agustinyedro@gmail.com>
2024-06-13 00:07:53 -0300

    se realiza el estilo bento de las imagenes

--- src/public/logic/imagenes.js
+++ src/public/logic/imagenes.js
@@ -1,32 +1,38 @@
 let imagesList = [];
 
 async function fetchImages() {
-    try {
-        const response = await fetch('/images');
-        if (!response.ok) {
-            throw new Error('Network response was not ok');
-        }
-        const images = await response.json();
-        imagesList = images; // Asigna los datos de imágenes a la variable imagesList
-        return images;
-    } catch (error) {
-        console.error('Error fetching images:', error);
-        return []; // Devuelve un array vacío si hay un error
+  try {
+    const response = await fetch("/images");
+    if (!response.ok) {
+      throw new Error("Network response was not ok");
     }
+    const images = await response.json();
+    imagesList = images; // Asigna los datos de imágenes a la variable imagesList
+    return images;
+  } catch (error) {
+    console.error("Error fetching images:", error);
+    return []; // Devuelve un array vacío si hay un error
+  }
 }
 
 async function renderImages() {
-    await fetchImages();
-    return  /* HTML */ `
-    <img src ="/img/cabañafrente.webp" alt = "cabaña" class="img-grande">
-        ${imagesList.map((image) => `<img src="/img/${image}" alt="${image}" class="img-pequenia">`).join('')}
-    `;
+  await fetchImages();
+
+  const classes = ["img-pequenia", "img-mediana", "img-grande"];
+
+  return /* HTML */ `
+    ${imagesList
+      .map(
+        (image, index) => /* HTML */ ` <div
+          class="imagen ${classes[index % classes.length]}"
+        >
+          <img src="/img/${image}" alt="${image}" />
+        </div>`
+      )
+      .join("")}
+  `;
 }
 
 export const galeriaImagenes = () => {
-    return renderImages();
-
+  return renderImages();
 };
-
-
-
